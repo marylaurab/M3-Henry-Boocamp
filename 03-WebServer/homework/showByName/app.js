8f7360c8a3422ac3de6fbbb947d98e3807c6f572
marylaurab <mbetancourt@fi.uba.ar>
2022-11-04 15:18:51 -0400

    webServer1

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,4 +1,19 @@
-var fs  = require("fs")
-var http  = require("http")
+var fs = require("fs");
+var http = require("http");
 
 // Escribí acá tu servidor
+http.createServer(function (req, res) {
+    //Desde el archivo (este) que escribo, es a partir hacia donde
+    //envio a buscar/leer el resto de la info.
+    //__dirname: ptm3/03weserver/hw/showByName
+    //Y por eos le agrego el resto que le falta /images/...etc
+    fs.readFile(`${__dirname}/images${req.url}.jpg`, (err,data) =>{
+        if(err) {
+            res.writeHead(404, {'Content-Type': 'text/plain'})
+            res.end("El perrito no se ha encontrado")
+        } else {
+            res.writeHead(200, {'Content-Type': "image/jpg"})
+            res.end(data)
+        }
+    }) 
+}).listen(3000, /*'127.0.0.1'*/'localhost');
