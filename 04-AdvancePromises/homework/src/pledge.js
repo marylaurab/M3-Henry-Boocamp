75b246c99b76eb381b7698fc9dc5fb116b2cbcb2
marylaurab <mbetancourt@fi.uba.ar>
2022-11-07 11:10:23 -0400

    Done! Falta ch4

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -1,10 +1,63 @@
-'use strict';
+"use strict";
+
 /*----------------------------------------------------------------
 Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
+function $Promise(executor) {
+  if (typeof executor !== "function")
+    throw new TypeError("the argument used as executor must be a function");
+  this._state = "pending";
+  this._value = undefined;
+  this._handlerGroups = [];
+  //arrow function:
+  executor(
+    (value) => this._internalResolve(value),
+    (reason) => this._internalReject(reason)
+  );
 
-
+  //usando bind:
+  //executor(this._internalResolve(this), this._internalReject(this))
+}
+$Promise.prototype._internalResolve = function (value) {
+  if (this._state === "pending") {
+    this._state = "fulfilled";
+    this._value = value;
+    //NO ENTIENDO ESTA LINEA AUN:
+    this._callHandlers();
+  }
+};
+$Promise.prototype._internalReject = function (reason) {
+  if (this._state === "pending") {
+    this._state = "rejected";
+    this._value = reason;
+    this._callHandlers();
+  }
+};
+$Promise.prototype.then = function (successHandler, errorHandler) {
+  if (typeof successHandler !== "function") successHandler = false;
+  if (typeof errorHandler !== "function") errorHandler = false;
+  this._handlerGroups.push({
+    successCb: successHandler,
+    errorCb: errorHandler,
+  });
+  if (this._state !== "pending") this._callHandlers();
+};
+$Promise.prototype._callHandlers = function () {
+  while (this._handlerGroups.length > 0) {
+    let actual = this._handlerGroups.shift();
+    if (this._state === "fulfilled" && actual.successCb) {
+      actual.successCb(this._value);
+    } else {
+      if (actual.errorCb) {
+        actual.errorCb(this._value);
+      }
+    }
+  }
+};
+$Promise.prototype.catch = function (errorHandler) {
+  this.then(null, errorHandler);
+};
 
 module.exports = $Promise;
 /*-------------------------------------------------------
