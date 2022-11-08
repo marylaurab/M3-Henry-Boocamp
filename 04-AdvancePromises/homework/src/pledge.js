46c2ca577aa058ae270b2368e587f523b6a2876e
marylaurab <mbetancourt@fi.uba.ar>
2022-11-08 10:24:18 -0400

    Faltan 4 test por pasar. Ch4

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -37,20 +37,61 @@ $Promise.prototype._internalReject = function (reason) {
 $Promise.prototype.then = function (successHandler, errorHandler) {
   if (typeof successHandler !== "function") successHandler = false;
   if (typeof errorHandler !== "function") errorHandler = false;
+  const downstreamPromise = new $Promise(function () {});
   this._handlerGroups.push({
     successCb: successHandler,
     errorCb: errorHandler,
+    downstreamPromise, //(poder anidar .then())
   });
   if (this._state !== "pending") this._callHandlers();
+  return downstreamPromise; //que la siguiente promesa devuelva una promesa
 };
 $Promise.prototype._callHandlers = function () {
   while (this._handlerGroups.length > 0) {
     let actual = this._handlerGroups.shift();
-    if (this._state === "fulfilled" && actual.successCb) {
-      actual.successCb(this._value);
+    if (this._state === "fulfilled") {
+      if (actual.successCb) {
+        try {
+          const result = actual.successCb(this._value);
+          if (result instanceof $Promise) {
+            result.then(
+              function (value) {
+                actual.downstreamPromise._internalResolve(value);
+              },
+              function (err) {
+                actual.downstreamPromise._internalReject(err);
+              }
+            );
+          } else {
+            actual.downstreamPromise._internalResolve(result);
+          }
+        } catch (e) {
+          actual.downstreamPromise._internalReject(e);
+        }
+      } else {
+        actual.downstreamPromise._internalResolve(this._value);
+      }
     } else {
       if (actual.errorCb) {
-        actual.errorCb(this._value);
+        try {
+          const result = actual.errorCb(this._value);
+          if (result instanceof $Promise) {
+            result.then(
+              function (value) {
+                actual.downstreamPromise._internalResolve(value);
+              },
+              function (err) {
+                actual.downstreamPromise._internalReject(err);
+              }
+            );
+          } else {
+            actual.downstreamPromise._internalResolve(result);
+          }
+        } catch (e) {
+          actual.downstreamPromise._internalReject(e);
+        }
+      } else {
+        actual.downstreamPromise._internalReject(this._value);
       }
     }
   }
