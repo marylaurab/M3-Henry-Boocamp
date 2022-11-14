9f4e8c945d7911fd48f27ad0a1be08208e94c136
marylaurab <mbetancourt@fi.uba.ar>
2022-11-14 12:56:51 -0400

    generator function* done

--- 07-AsyncAwait/Generators/homework/homework.js
+++ 07-AsyncAwait/Generators/homework/homework.js
@@ -1,6 +1,33 @@
-function fizzBuzzGenerator(max) {
+function* fizzBuzzGenerator(max) {
   // Tu código acá:
-
+  let i = 1;
+  //if (max) {
+  while (max ? i <= max : true) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      yield "Fizz Buzz";
+    } else if (i % 3 === 0) {
+      yield "Fizz";
+    } else if (i % 5 === 0) {
+      yield "Buzz";
+    } else {
+      yield i;
+    }
+    i++;
+  }
+  //} else {
+  //   while (true) {
+  //     if (i % 3 === 0 && i % 5 === 0) {
+  //       yield 'Fizz Buzz';
+  //     } else if (i % 3 === 0 && i % 5 !== 0) {
+  //       yield 'Fizz';
+  //     } else if (i % 3 !== 0 && i % 5 === 0) {
+  //       yield 'Buzz';
+  //     } else {
+  //       yield i;
+  //     }
+  //     i++;
+  //   }
+  // }
 }
 
 module.exports = fizzBuzzGenerator;
